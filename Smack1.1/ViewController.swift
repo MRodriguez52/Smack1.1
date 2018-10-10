2b1d9398ebd8ee5ff5f41025bff09eb21e21379b
Miguel Rodriguez <miguelrodriguez@miguels-macbook-pro-2.local>
2018-10-10 12:39:19 -0500

    Initial Commit

---
+++ Smack1.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack1.1
+//
+//  Created by Miguel Rodriguez on 10/10/18.
+//  Copyright © 2018 Miguel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
